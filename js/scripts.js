0f7698745aa20a518cb55435ae07766413395c49
Katelyn <115312112+Kmart99@users.noreply.github.com>
2022-12-12 11:07:44 -0700

    2.3 Submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello! Welcome to my portfolio!")
+// Hamburger menu script
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+}
